433a81d79a27bf381b75ad14f7cfa19801c15e5d
blqnce <eanavass02@gmail.com>
2026-09-08 21:48:13 -0500

    feat: Implementación de Capa de Datos con Firebase y Hilt

---
+++ app/src/main/java/movil/ratemypc/data/repository/AuthRepository.kt
@@ -0,0 +1,27 @@
+package movil.ratemypc.data.repository
+
+import com.google.firebase.auth.FirebaseUser
+import movil.ratemypc.data.datasource.AuthRemoteDataSource
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor(
+    private val authRemoteDataSource: AuthRemoteDataSource
+
+){
+
+    val currentUser: FirebaseUser? = authRemoteDataSource.currentUser
+
+    suspend fun signIn(email: String, password: String){
+        authRemoteDataSource.signIn(email,password)
+    }
+
+    suspend fun signUp(email: String,password: String){
+        authRemoteDataSource.signUp(email,password)
+    }
+
+    fun signOut(){
+        authRemoteDataSource.signOut()
+    }
+
+
+}
